dec747e360dce6b49f79126476690165b5e33ecc
Rahmaahmed685 <rahmaahmed8276@email.com>
2023-09-26 10:09:48 +0300

    untill add fav screen

--- lib/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color contaierColor = Color(0XFFd0f4de);
-const Color mainColor = Color(0xFF2a9d8f);
-const Color deleteColor = Color(0xFFe76f51);
-const Color editColor = Color(0xFF2a9d8f);
-const Color backGround = Color(0XFFfefae0);
